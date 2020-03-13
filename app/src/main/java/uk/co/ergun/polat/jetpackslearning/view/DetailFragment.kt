0cdd6769c4c557a2e469ab94bebd7aed3fc42762
Ergun Polat <ergun.polat@sky.uk>
2020-03-13 16:30:22 +0000

    set background based on image colour in detail view

--- app/src/main/java/uk/co/ergun/polat/jetpackslearning/view/DetailFragment.kt
+++ app/src/main/java/uk/co/ergun/polat/jetpackslearning/view/DetailFragment.kt
@@ -1,11 +1,17 @@
 package uk.co.ergun.polat.jetpackslearning.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 import uk.co.ergun.polat.jetpackslearning.R
@@ -35,11 +41,33 @@ class DetailFragment : Fragment() {
 
         context?.let {
             animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
-            animalName.text = animal?.name
-            animalLocation.text = animal?.location
-            animalLifeSpan.text = animal?.lifeSpan
-            animalDiet.text = animal?.diet
         }
 
+        animalName.text = animal?.name
+        animalLocation.text = animal?.location
+        animalLifeSpan.text = animal?.lifeSpan
+        animalDiet.text = animal?.diet
+
+        animal?.imageUrl?.let {
+            setUpBackgroundColour(it)
+        }
+
+
+    }
+
+    private fun setUpBackgroundColour(url: String) {
+        Glide.with(this).asBitmap().load(url).into(object : CustomTarget<Bitmap>() {
+            override fun onLoadCleared(placeholder: Drawable?) {
+
+            }
+
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                Palette.from(resource).generate { palette ->
+                    val intColour = palette?.lightMutedSwatch?.rgb ?: 0
+                    animalLayout.setBackgroundColor(intColour)
+                }
+            }
+
+        })
     }
 }
